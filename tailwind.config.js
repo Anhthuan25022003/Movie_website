766c9a8d86261ca1c2bab616c70dd7e18173ff59
Anhthuan25022003 <thuanit25223@gmail.com>
2024-10-21 22:43:27 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'banner' : "url('/banner.png')",
+      }
+    },
+  },
+  plugins: [],
+}
+
